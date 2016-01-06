d95bedbbe0176450cc671aca0991b48f2a2619f1
CarlosRA97 <carlosriveroaro7@gmail.com>
2016-01-06 20:05:44 +0100

    update

--- gpio.py
+++ gpio.py
@@ -1,15 +1,20 @@
 from RPi.GPIO import *
+from ventanas import Ventana
 
-setmode(BCM)
-setwarnings(False)
+# setmode(BCM)
+# setwarnings(False)
 
-led = input("Escribe el pin: ")
+# led = input("Escribe el pin: ")
 
-setup(led, OUT)
+# setup(led, OUT)
 
-onoff = raw_input("On or Off: ")
+# onoff = raw_input("On or Off: ")
 
-if onoff == "on":
-        output(led, 1)
-elif onoff == "off":
-        output(led, 0)
+# if onoff == "on":
+#         output(led, 1)
+# elif onoff == "off":
+#         output(led, 0)
+
+ventana1 = Ventana()
+
+ventana1.w()
---
+++ ventanas.py
@@ -0,0 +1,29 @@
+#Crea la ventana
+class Ventana(object):
+	def w():
+		raiz = Tk()
+
+		#Modifica la ventana y lo que hay dentro
+		raiz.title('Sha1 Generator')
+		raiz.geometry('195x108')
+
+		    #Ventana
+		ventana = Frame(raiz)
+		ventana.grid(column=0, row=0, padx=(10,50), pady=(10,10))
+		ventana.columnconfigure(0,weight=1)
+		ventana.rowconfigure(0,weight=1)
+
+		    #Etiquetas de texto
+		tag = Label(ventana, text="Texto a encriptar")
+		tag.grid(column=2, row=1, pady=(5,5))
+
+		    #Botones
+		button = Button(ventana, text="Hash it!", command=hacer_click)
+		button.grid(column=2, row=3, padx=(5,5), pady=(5,5))
+
+		    #Campo de entrada de texto
+		entrada_txt = Entry(ventana, width=20, textvariable="")
+		entrada_txt.grid(column=2, row=2)
+
+		#Poner en marcha el ciclo de eventos
+		raiz.mainloop()
