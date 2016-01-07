8dda2c5e8697421a70c1e1320bcb43fbed3958d0
CarlosRA97 <carlosriveroaro7@gmail.com>
2016-01-07 16:11:29 +0100

    update

--- ventanas.py
+++ ventanas.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+import tkMessageBox
 from RPi.GPIO import *
 
 lista = [2,3,4,17,27,22,10,9,11,14,15,18,23,24,25,8,7]
@@ -8,10 +9,10 @@ setmode(BCM)
 setwarnings(False)
 
 def onoff(pin):
-	int(pin)
 	print pin
 
-
+def pinCallBack(pin):
+	tkMessageBox.showinfo("You clicked the pin: %s" % pin)
 
 
 #Crea la ventana
@@ -36,38 +37,38 @@ tag.grid(column=1, row=1, pady=(5,5))
 button3v3 = Button(ventana, text="3v3")
 button3v3.grid(column=1, row=2, padx=(5,5), pady=(5,5))
 
-button2 = Button(ventana, text="2|SDA", command = onoff(2))
+button2 = Button(ventana, text="2|SDA", command = pinCallBack(2))
 button2.grid(column=1, row=3, padx=(5,5), pady=(5,5))
 
 
-button3 = Button(ventana, text="3|SCL", command = onoff(3))
+button3 = Button(ventana, text="3|SCL", command = pinCallBack(3))
 button3.grid(column=1, row=4, padx=(5,5), pady=(5,5))
 
-button4 = Button(ventana, text="4", command = onoff(4))
+button4 = Button(ventana, text="4", command = pinCallBack(4))
 button4.grid(column=1, row=5, padx=(5,5), pady=(5,5))
 
 buttongnd = Button(ventana, text="GND")
 buttongnd.grid(column=1, row=6, padx=(5,5), pady=(5,5))
 
-button17 = Button(ventana, text="17", command = onoff(17))
+button17 = Button(ventana, text="17", command = pinCallBack(17))
 button17.grid(column=1, row=7, padx=(5,5), pady=(5,5))
 
-button27 = Button(ventana, text="27", command = onoff(27))
+button27 = Button(ventana, text="27", command = pinCallBack(27))
 button27.grid(column=1, row=8, padx=(5,5), pady=(5,5))
 
-button22 = Button(ventana, text="22", command = onoff(22))
+button22 = Button(ventana, text="22", command = pinCallBack(22))
 button22.grid(column=1, row=9, padx=(5,5), pady=(5,5))
 
 button3v32 = Button(ventana, text="3v3")
 button3v32.grid(column=1, row=10, padx=(5,5), pady=(5,5))
 
-button10 = Button(ventana, text="10|MOSI", command = onoff(10))
+button10 = Button(ventana, text="10|MOSI", command = pinCallBack(10))
 button10.grid(column=1, row=11, padx=(5,5), pady=(5,5))
 
-button9 = Button(ventana, text="9|MISO", command = onoff(9))
+button9 = Button(ventana, text="9|MISO", command = pinCallBack(9))
 button9.grid(column=1, row=12, padx=(5,5), pady=(5,5))
 
-button11 = Button(ventana, text="11|SCLK", command = onoff(11))
+button11 = Button(ventana, text="11|SCLK", command = pinCallBack(11))
 button11.grid(column=1, row=13, padx=(5,5), pady=(5,5))
 
 buttongnd2 = Button(ventana, text="GND")
@@ -85,34 +86,34 @@ button5v2.grid(column=2, row=3, padx=(5,5), pady=(5,5))
 buttongnd3 = Button(ventana, text="GND")
 buttongnd3.grid(column=2, row=4, padx=(5,5), pady=(5,5))
 
-button14 = Button(ventana, text="14|TXD", command = onoff(14))
+button14 = Button(ventana, text="14|TXD", command = pinCallBack(14))
 button14.grid(column=2, row=5, padx=(5,5), pady=(5,5))
 
-button15 = Button(ventana, text="15|RXD", command = onoff(15))
+button15 = Button(ventana, text="15|RXD", command = pinCallBack(15))
 button15.grid(column=2, row=6, padx=(5,5), pady=(5,5))
 
-button18 = Button(ventana, text="18", command = onoff(18))
+button18 = Button(ventana, text="18", command = pinCallBack(18))
 button18.grid(column=2, row=7, padx=(5,5), pady=(5,5))
 
 buttongnd4 = Button(ventana, text="GND")
 buttongnd4.grid(column=2, row=8, padx=(5,5), pady=(5,5))
 
-button23 = Button(ventana, text="23", command = onoff(23))
+button23 = Button(ventana, text="23", command = pinCallBack(23))
 button23.grid(column=2, row=9, padx=(5,5), pady=(5,5))
 
-button24 = Button(ventana, text="24", command = onoff(24))
+button24 = Button(ventana, text="24", command = pinCallBack(24))
 button24.grid(column=2, row=10, padx=(5,5), pady=(5,5))
 
 buttongnd5 = Button(ventana, text="GND")
 buttongnd5.grid(column=2, row=11, padx=(5,5), pady=(5,5))
 
-button25 = Button(ventana, text="25", command = onoff(25))
+button25 = Button(ventana, text="25", command = pinCallBack(25))
 button25.grid(column=2, row=12, padx=(5,5), pady=(5,5))
 
-button8 = Button(ventana, text="8|CE0", command = onoff(8))
+button8 = Button(ventana, text="8|CE0", command = pinCallBack(8))
 button8.grid(column=2, row=13, padx=(5,5), pady=(5,5))
 
-button7 = Button(ventana, text="7|CE1", command = onoff(7))
+button7 = Button(ventana, text="7|CE1", command = pinCallBack(7))
 button7.grid(column=2, row=14, padx=(5,5), pady=(5,5))
 
 
