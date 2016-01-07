ea47fce369a0d2421a9dd3efd3aeb968789d9665
CarlosRA97 <carlosriveroaro7@gmail.com>
2016-01-07 17:23:53 +0100

    update

--- ventanas.py
+++ ventanas.py
@@ -11,8 +11,10 @@ setwarnings(False)
 def onoff(pin):
 	print pin
 
-def pinCallBack():
+def OnButtonClick(self, button_id):
+	print button_id
 	tkMessageBox.showinfo("You clicked the pin")
+
 
 
 #Crea la ventana
@@ -37,38 +39,38 @@ tag.grid(column=1, row=1, pady=(5,5))
 button3v3 = Button(ventana, text="3v3")
 button3v3.grid(column=1, row=2, padx=(5,5), pady=(5,5))
 
-button2 = Button(ventana, text="2|SDA", command = pinCallBack)
+self.button2 = Button(vself,entana, text="2|SDA", command =lambda: self.OnButtonClick(2))
 button2.grid(column=1, row=3, padx=(5,5), pady=(5,5))
 
 
-button3 = Button(ventana, text="3|SCL", command = pinCallBack)
+self.button3 = Button(vself,entana, text="3|SCL", command =lambda: self.OnButtonClick(3))
 button3.grid(column=1, row=4, padx=(5,5), pady=(5,5))
 
-button4 = Button(ventana, text="4", command = pinCallBack)
+self.button4 = Button(vself,entana, text="4", command =lambda: self.OnButtonClick(4))
 button4.grid(column=1, row=5, padx=(5,5), pady=(5,5))
 
 buttongnd = Button(ventana, text="GND")
 buttongnd.grid(column=1, row=6, padx=(5,5), pady=(5,5))
 
-button17 = Button(ventana, text="17", command = pinCallBack)
+self.button17 = Button(self,ventana, text="17", command =lambda: self.OnButtonClick(17))
 button17.grid(column=1, row=7, padx=(5,5), pady=(5,5))
 
-button27 = Button(ventana, text="27", command = pinCallBack)
+self.button27 = Button(self,ventana, text="27", command =lambda: self.OnButtonClick(27))
 button27.grid(column=1, row=8, padx=(5,5), pady=(5,5))
 
-button22 = Button(ventana, text="22", command = pinCallBack)
+self.button22 = Button(self,ventana, text="22", command =lambda: self.OnButtonClick(22))
 button22.grid(column=1, row=9, padx=(5,5), pady=(5,5))
 
 button3v32 = Button(ventana, text="3v3")
 button3v32.grid(column=1, row=10, padx=(5,5), pady=(5,5))
 
-button10 = Button(ventana, text="10|MOSI", command = pinCallBack)
+self.button10 = Button(self,ventana, text="10|MOSI", command =lambda: self.OnButtonClick(10))
 button10.grid(column=1, row=11, padx=(5,5), pady=(5,5))
 
-button9 = Button(ventana, text="9|MISO", command = pinCallBack)
+self.button9 = Button(vself,entana, text="9|MISO", command =lambda: self.OnButtonClick(9))
 button9.grid(column=1, row=12, padx=(5,5), pady=(5,5))
 
-button11 = Button(ventana, text="11|SCLK", command = pinCallBack)
+self.button11 = Button(self,ventana, text="11|SCLK", command =lambda: self.OnButtonClick(11))
 button11.grid(column=1, row=13, padx=(5,5), pady=(5,5))
 
 buttongnd2 = Button(ventana, text="GND")
@@ -86,34 +88,34 @@ button5v2.grid(column=2, row=3, padx=(5,5), pady=(5,5))
 buttongnd3 = Button(ventana, text="GND")
 buttongnd3.grid(column=2, row=4, padx=(5,5), pady=(5,5))
 
-button14 = Button(ventana, text="14|TXD", command = pinCallBack)
+self.button14 = Button(self,ventana, text="14|TXD", command =lambda: self.OnButtonClick(14))
 button14.grid(column=2, row=5, padx=(5,5), pady=(5,5))
 
-button15 = Button(ventana, text="15|RXD", command = pinCallBack)
+self.button15 = Button(self,ventana, text="15|RXD", command =lambda: self.OnButtonClick(15))
 button15.grid(column=2, row=6, padx=(5,5), pady=(5,5))
 
-button18 = Button(ventana, text="18", command = pinCallBack)
+self.button18 = Button(self,ventana, text="18", command =lambda: self.OnButtonClick(18))
 button18.grid(column=2, row=7, padx=(5,5), pady=(5,5))
 
 buttongnd4 = Button(ventana, text="GND")
 buttongnd4.grid(column=2, row=8, padx=(5,5), pady=(5,5))
 
-button23 = Button(ventana, text="23", command = pinCallBack)
+self.button23 = Button(self,ventana, text="23", command =lambda: self.OnButtonClick(23))
 button23.grid(column=2, row=9, padx=(5,5), pady=(5,5))
 
-button24 = Button(ventana, text="24", command = pinCallBack)
+self.button24 = Button(self,ventana, text="24", command =lambda: self.OnButtonClick(24))
 button24.grid(column=2, row=10, padx=(5,5), pady=(5,5))
 
 buttongnd5 = Button(ventana, text="GND")
 buttongnd5.grid(column=2, row=11, padx=(5,5), pady=(5,5))
 
-button25 = Button(ventana, text="25", command = pinCallBack)
+self.button25 = Button(self,ventana, text="25", command =lambda: self.OnButtonClick(25))
 button25.grid(column=2, row=12, padx=(5,5), pady=(5,5))
 
-button8 = Button(ventana, text="8|CE0", command = pinCallBack)
+self.button8 = Button(vself,entana, text="8|CE0", command =lambda: self.OnButtonClick(8))
 button8.grid(column=2, row=13, padx=(5,5), pady=(5,5))
 
-button7 = Button(ventana, text="7|CE1", command = pinCallBack)
+self.button7 = Button(vself,entana, text="7|CE1", command =lambda: self.OnButtonClick(7))
 button7.grid(column=2, row=14, padx=(5,5), pady=(5,5))
 
 
