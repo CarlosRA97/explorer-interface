5d5ba407b22a91020c76c73e21114c96675ca1ff
Carlos Rivero Aro <Carlos@MacBook-Pro-de-Carlos.local>
2016-01-30 14:20:07 +0100

    renovación

--- ventanas.py
+++ ventanas.py
@@ -1,27 +1,47 @@
+#!/usr/bin/env python2.7
+
 from Tkinter import *
 import tkMessageBox
 from RPi.GPIO import *
+# from wiringpi2 import *
+from time import sleep
 
-lista = [2,3,4,17,27,22,10,9,11,14,15,18,23,24,25,8,7]
-
+pinesGPIO = [2,3,4,17,27,22,10,9,11,14,15,18,23,24,25,8,7]
 
 setmode(BCM)
 setwarnings(False)
 
-def on(pin):
+def stp(pin):
 	setup(pin, OUT)
-	output(pin,1)
+
+def on(pin):
+    output(pin,1)
+
 
 def off(pin):
-	setup(pin, OUT)
-	output(pin,0)
+    output(pin,0)
+
+
+def repeat(pin,times):
+    counter = 0
+    while times >= counter:
+        output(pin,1)
+        sleep(.1)
+        output(pin,0)
+        sleep(.1)
+        counter += 1
+
 
 def OnButtonClick(button):
 	result = tkMessageBox.askyesno("GPIO PIN","Do you want pin %s turn on?" % button)
 	if result:
+		stp(button)
 		on(button)
 	else:
+		stp(button)
+		repeat(button,4)
 		off(button)
+
 
 
 #Crea la ventana
@@ -38,114 +58,41 @@ ventana.rowconfigure(0,weight=1)
 
 #Etiquetas de texto
 tag = Label(ventana, text="Pines")
-tag.grid(column=1, row=1, pady=(5,5))
+tag.grid(column=2, row=1, pady=(5,5))
 
-#Botones
+#------------Botones-----------#
+def botonera():
 
-# Primera columna
-button3v3 = Button(ventana, text="3v3")
-button3v3.grid(column=1, row=2, padx=(5,5), pady=(5,5))
+    rowGrid = 2
+    colGrid = 1
 
-button2 = Button(ventana, text="2|SDA")
-button2.grid(column=1, row=3, padx=(5,5), pady=(5,5))
-button2.config(command = lambda: OnButtonClick(2))
+    # Primera columna
 
-button3 = Button(ventana, text="3|SCL")
-button3.grid(column=1, row=4, padx=(5,5), pady=(5,5))
-button3.config(command = lambda: OnButtonClick(3))
+    for pin in pinesGPIO[:1+len(pinesGPIO)/2]:
+        _ = Button(ventana, text=str(pin))
+        _.grid(column=colGrid, row=rowGrid, padx=(5,5), pady=(5,5))
+        _.config(command = lambda: OnButtonClick(pin))
 
-button4 = Button(ventana, text="4")
-button4.grid(column=1, row=5, padx=(5,5), pady=(5,5))
-button4.config(command = lambda: OnButtonClick(4))
+        rowGrid += 1
 
-buttongnd = Button(ventana, text="GND")
-buttongnd.grid(column=1, row=6, padx=(5,5), pady=(5,5))
+    # Segunda columna
 
-button17 = Button(ventana, text="17")
-button17.grid(column=1, row=7, padx=(5,5), pady=(5,5))
-button17.config(command = lambda: OnButtonClick(17))
+    colGrid = 3
+    rowGrid = 2
 
-button27 = Button(ventana, text="27")
-button27.grid(column=1, row=8, padx=(5,5), pady=(5,5))
-button27.config(command = lambda: OnButtonClick(27))
+    for pin in pinesGPIO[1+len(pinesGPIO)/2:]:
+        _ = Button(ventana, text=str(pin))
+        _.grid(column=colGrid, row=rowGrid, padx=(5,5), pady=(5,5))
+        _.config(command = lambda: OnButtonClick(pin))
 
-button22 = Button(ventana, text="22")
-button22.grid(column=1, row=9, padx=(5,5), pady=(5,5))
-button22.config(command = lambda: OnButtonClick(22))
+        rowGrid += 1
 
-button3v32 = Button(ventana, text="3v3")
-button3v32.grid(column=1, row=10, padx=(5,5), pady=(5,5))
-
-button10 = Button(ventana, text="10|MOSI")
-button10.grid(column=1, row=11, padx=(5,5), pady=(5,5))
-button10.config(command = lambda: OnButtonClick(10))
-
-button9 = Button(ventana, text="9|MISO")
-button9.grid(column=1, row=12, padx=(5,5), pady=(5,5))
-button9.config(command = lambda: OnButtonClick(9))
-
-button11 = Button(ventana, text="11|SCLK")
-button11.grid(column=1, row=13, padx=(5,5), pady=(5,5))
-button11.config(command = lambda: OnButtonClick(11))
-
-buttongnd2 = Button(ventana, text="GND")
-buttongnd2.grid(column=1, row=14, padx=(5,5), pady=(5,5))
-
-
-# Segunda columna
-
-button5v = Button(ventana, text="5v")
-button5v.grid(column=2, row=2, padx=(5,5), pady=(5,5))
-
-button5v2 = Button(ventana, text="5v")
-button5v2.grid(column=2, row=3, padx=(5,5), pady=(5,5))
-
-buttongnd3 = Button(ventana, text="GND")
-buttongnd3.grid(column=2, row=4, padx=(5,5), pady=(5,5))
-
-button14 = Button(ventana, text="14|TXD")
-button14.grid(column=2, row=5, padx=(5,5), pady=(5,5))
-button14.config(command = lambda: OnButtonClick(14))
-
-button15 = Button(ventana, text="15|RXD")
-button15.grid(column=2, row=6, padx=(5,5), pady=(5,5))
-button15.config(command = lambda: OnButtonClick(15))
-
-button18 = Button(ventana, text="18")
-button18.grid(column=2, row=7, padx=(5,5), pady=(5,5))
-button18.config(command = lambda: OnButtonClick(18))
-
-buttongnd4 = Button(ventana, text="GND")
-buttongnd4.grid(column=2, row=8, padx=(5,5), pady=(5,5))
-
-button23 = Button(ventana, text="23")
-button23.grid(column=2, row=9, padx=(5,5), pady=(5,5))
-button23.config(command = lambda: OnButtonClick(23))
-
-button24 = Button(ventana, text="24")
-button24.grid(column=2, row=10, padx=(5,5), pady=(5,5))
-button24.config(command = lambda: OnButtonClick(24))
-
-buttongnd5 = Button(ventana, text="GND")
-buttongnd5.grid(column=2, row=11, padx=(5,5), pady=(5,5))
-
-button25 = Button(ventana, text="25")
-button25.grid(column=2, row=12, padx=(5,5), pady=(5,5))
-button25.config(command = lambda: OnButtonClick(25))
-
-button8 = Button(ventana, text="8|CE0")
-button8.grid(column=2, row=13, padx=(5,5), pady=(5,5))
-button8.config(command = lambda: OnButtonClick(8))
-
-button7 = Button(ventana, text="7|CE127")
-button7.grid(column=2, row=14, padx=(5,5), pady=(5,5))
-button7.config(command = lambda: OnButtonClick(7))
-
-
+#------------------------------#
 
 #Campo de entrada de texto
 # entrada_txt = Entry(ventana, width=20, textvariable="")
 # entrada_txt.grid(column=2, row=2)
 
-#Poner en marcha el ciclo de eventos
+#Poner en marcha el ciclo de eventos y llama a todas las funciones visuales
+botonera()
 raiz.mainloop()
