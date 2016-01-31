7b9028256da0616adb4f29076a5ef0da7321c4d2
Carlos Rivero Aro <Carlos@MacBook-Pro-de-Carlos.local>
2016-01-31 14:47:00 +0100

    corregiendo

--- Imajenes de camara color y gris.py
+++ Imajenes de camara color y gris.py
@@ -11,12 +11,12 @@ import numpy as np
 cap = cv2.VideoCapture(1)
 #esperamos a que la camara este trasmitiendo
 while True:
-    ret, frame = cap.read() # rent?="refrecar" creamos un opjeto "frame" con las imajenes de la camara
-    gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #la umajen en color BGR(azul berde rojo) se pasan a escala de grises
+    ret, frame = cap.read() # rent?="refrecar" creamos un objeto "frame" con las imagenes de la camara
+    gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #la imajen en color BGR(azul, verde, rojo) se pasan a escala de grises
 
-    cv2.imshow('fame', frame)#mostramos las imajenes en una ventana de titulo "fame"
-    cv2.imshow('gris', gris) #muestra la imajen en gris "..."
-    #creamos un condicionalpra cerrar la ventana al presionar la q con un ciclo de espera
+    cv2.imshow('fame', frame) # mostramos las imajenes en una ventana de titulo "fame"
+    cv2.imshow('gris', gris)  # muestra la imajen en gris "..."
+    # creamos un condicional para cerrar la ventana al presionar la q con un ciclo de espera
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release() #mostramos
