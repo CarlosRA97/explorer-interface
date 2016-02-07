57916fb3aea0249117dd06a89a91ad4f0a169104
Carlos Rivero Aro <Carlos@MacBook-Pro-de-Carlos.local>
2016-02-07 13:41:41 +0100

    cambiando a clases

--- calling.py
+++ calling.py
@@ -3,4 +3,15 @@ from fibonacci import *
 
 intro = input("Introduce un numero: ")
 
-fib(intro)
+newfibo = fibo(intro)
+
+nu = 13
+
+if 55 in newfibo.array():
+    print newfibo.lmun()
+    print "lo encontre el 55"
+elif nu in newfibo.array():
+    print newfibo.lnum()
+    print "lo encontre el %s" % nu
+else:
+    print "no se encuentran en dicha serie"
--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,17 @@
-def fib(n):    # escribe la serie Fibonacci hasta n
-    a, b = 0, 1
-    while b < n:
-        print b,
-        a, b = b, a+b
+class Calc(object):
+    def __init__(self,number):
+        self.number = number
 
-def fib2(n): # devuelve la serie Fibonacci hasta n
-    resultado = []
-    a, b = 0, 1
-    while b < n:
-        resultado.append(b)
-        a, b = b, a+b
-    return resultado
+    def fib(self):    # escribe la serie Fibonacci hasta n
+        a, b = 0, 1
+        while b < self.number:
+            print b,
+            a, b = b, a+b
+
+    def fib2(self): # devuelve la serie Fibonacci hasta n
+        resultado = []
+        a, b = 0, 1
+        while b < self.number:
+            resultado.append(b)
+            a, b = b, a+b
+        return resultado
