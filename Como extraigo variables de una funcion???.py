5cbb31cd514432fe6d34b4131140d13fd56cc8f0
carl.riv.aro <carl.riv.aro@outlook.com>
2016-02-08 11:39:46 -0500

    Mejor sin clases

--- Como extraigo variables de una funcion???.py
+++ Como extraigo variables de una funcion???.py
@@ -30,6 +30,8 @@ def squareOnClick(ix,iy):
 
 
 # Create a black image, a window and bind the function to window
+
+
 img = np.zeros((512,512,3), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',mouse)
