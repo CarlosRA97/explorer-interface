ff9585c7312bcaa6b913c2cd544b606a830d85eb
Carlos Rivero Aro <Carlos@MacBook-Pro-de-Carlos.local>
2016-02-25 01:24:08 +0100

    asi dura más tiempo. se cae la app por falta de ram en el ordenador y por la asignación de cantidad de ram a consumir para ese programa en especifico y cuantas menos variables uses mejor ya que cada vez que estableces una variable estas guardando un hueco en la memoria y si ya lo estas obteniendo no lo vuelvas a guardar en otra variable podiendo obtener los datos directamente

--- Color.1.0.py
+++ Color.1.0.py
@@ -1,40 +1,41 @@
 import cv2
 import numpy as np
-ix,iy= 0,0
+import sys
+
+sys.settrace
 
 
 cap = cv2.VideoCapture(0)
 def detecting_pixels(event,x,y,flags,param):
-    if event == cv2.EVENT_MOUSEMOVE:
-                ix,iy = x,y
-                print 'Coordenada X= (%i) Y= (%i)' % (ix,iy)
-    elif event == cv2.EVENT_LBUTTONDOWN:
-        #analisisPix(ix,iy)
-        print 'Pixele selecionado(%i)(%i)' % (ix,iy)
+    # if event == cv2.EVENT_MOUSEMOVE:
+    #     print 'Coordenada X= (%i) Y= (%i)' % (x,y)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # analisisPix(x,y)
+        print 'Pixele selecionado (%i, %i)' % (x,y)
 
 #def analisisPix(ix,iy):
     #px = frame[ix,iy]
 
 while(1):
 
-#Bolque 1: captura y tratamiento de la camar
+#Bolque 1: captura y tratamiento de la camara
     #Take each frame
     _, frame = cap.read()
 
     #Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-#Bloque 2: variables de colo
+#Bloque 2: variables de color
     lower_blue = np.array([0,51,25])
 
     upper_blue = np.array([153,255,153])
 
-#Boloque 3:Creacion de masaras
+#Bloque 3:Creacion de mascaras
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
-#Bloque 4: muestra de las macaras
+#Bloque 4: muestra de las mascaras
 
     #cv2.imshow('mask',mask)
     #cv2.imshow('res',res)
