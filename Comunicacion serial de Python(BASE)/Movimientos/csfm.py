44e29c7a5ac70bd0a0123f215d7034066e88ea9f
Carlos Rivero Aro <Carlos@MacBook-Pro-de-Carlos.local>
2016-03-21 20:37:03 +0100

    ostia

--- Comunicacion serial de Python(BASE)/Movimientos/csfm.py
+++ Comunicacion serial de Python(BASE)/Movimientos/csfm.py
@@ -6,13 +6,17 @@ if Ser.is_open == True:
     pass
 
 class Movimiento():
-    def __init__(self):
-        print('Movimiento funciona')
+    def __init__(self, velocidad):
+        self.velocidad = velocidad
 
     def avance(self):
         Ser.open()
         Ser.write('a')
         Ser.close()
+
+    def retrocder(self):
+        pass
+
 
 
 if __name__ == '__main__':
